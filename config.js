c972941246d1177cb1c480ae3dddab6826d1d2b7
Eli Cooper <elicooper18@gmail.com>
2014-09-29 15:12:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,3 @@
-// var yelp = {
-// 	consumerKey: 'JLVd75bgPbDkt7njQV6YAA',
-// 	consumerSecret: 'qWlLQu2Vm6iPLmGgzSct6b8bEUI',
-// 	accessToken: 'JHRpiBUyG4vlz6To7RVj3MxEx03QoWDK',
-// 	accessTokenSecret: 'xNBS22ziCyyOYYpNmdahc52fsDw',
-// 	serviceProvider: {
-// 		signatureMethod: 'HMAC-SHA1'
-// 	}
-// };
-
-// var flickr = {
-// 	key: '34391d62a5636661d9068d49a2f3266a',
-// 	secret: '78e601a5d841b313'
-// };
-
 var google = {
 	api_key: 'AIzaSyAqTm9EZQ64RIP1as6cCw4Ek4ak026gchE'
 };
